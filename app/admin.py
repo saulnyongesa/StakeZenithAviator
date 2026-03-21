ab13044205e063298f7f73a3ea7501cab187c59c
saulnyongesa <saulnyongesa4@gmail.com>
2026-03-21 17:33:50 +0300

    Admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from app.models import AviatorBet, AviatorDeposit, AviatorProfile
+
 # Register your models here.
+admin.site.register(AviatorProfile)
+admin.site.register(AviatorDeposit)
+admin.site.register(AviatorBet)
